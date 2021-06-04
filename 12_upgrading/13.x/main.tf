80951874d97866fefb018a10e3604b9fadb5cce6
Chris Guest <chris.guest@condenastint.com>
2021-06-04 19:05:59 +0100

    Up to version 13

--- 12_upgrading/13.x/main.tf
+++ 12_upgrading/13.x/main.tf
@@ -1,8 +1,15 @@
 terraform {
-  required_version = "=0.11.15"
+  required_version = "=0.13.7"
 
   backend "local" {
     path = "../state/terraform.tfstate"
+  }
+
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.12.1"
+    }
   }
 }
 
@@ -17,22 +24,22 @@ locals {
 
 resource "local_file" "file1" {
   content  = "This is my file1"
-  filename = "${local.path1}"
+  filename = local.path1
 }
 
 resource "local_file" "file2" {
   content  = "This is my file2"
-  filename = "${local.path2}"
+  filename = local.path2
 }
 
 resource "docker_container" "container1" {
-  name    = "${local.name1}"
+  name    = local.name1
   image   = "redis:6.2-alpine"
   restart = "always"
 }
 
 resource "docker_container" "container2" {
-  name    = "${local.name2}"
+  name    = local.name2
   image   = "redis:6.2-alpine"
   restart = "always"
 }
--- 12_upgrading/README.md
+++ 12_upgrading/README.md
@@ -1,23 +1,42 @@
 # README
 Demonstrate how to upgrade terraform state to latest version
 
-## Create
+tfenv list-remote
+tfenv install 0.11.15
+## Create 0.11.x
 ```sh
 cd 11.x
+tfenv use 0.11.15
 terraform init
-
+terraform fmt
+terraform validate
 terraform plan 
-
-# apply the plan
 terraform apply -auto-approve
-
-# print out the container id
-terraform output
 ```
 
+## Create 0.12.x
+```sh
+cd ../12.x
+tfenv use 0.12.31
+code --diff ./main.tf ../11.x/main.tf  
+terraform init
+terraform fmt
+terraform validate
+terraform plan 
+terraform apply -auto-approve
+```
 
-tfenv list-remote
+## Create 0.13.x
+```sh
+cd ../13.x
+tfenv use 0.13.7
+code --diff ./main.tf ../12.x/main.tf  
+terraform init
+terraform fmt
+terraform validate
+terraform plan 
+terraform apply -auto-approve
+```
 
 tfenv use 0.15.5
 tfenv use 0.14.11
-tfenv use 0.13.7
