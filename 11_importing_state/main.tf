6dcad4cd4f96d5656f6dc6c80b09e1081a8dcb08
Chris Guest <chris.guest@condenastint.com>
2021-06-05 15:14:24 +0100

    Importing example

---
+++ 11_importing_state/main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_version = "=0.15.05"
+
+  backend "local" {
+    path = "./state/terraform.tfstate"
+  }
+
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.12.1"
+    }
+  }
+}
+
+provider "docker" {}
+
+locals {
+  containers = [
+    {
+      name : "nginx1"
+      ports : {
+        internal : "80"
+        external : "8080"
+      }
+    }
+  ]
+}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:1.20.1"
+  keep_locally = true
+}
+
+resource "docker_container" "container" {
+  # strange logic seems to be so resource has a name
+  for_each = { for container in local.containers : container.name => container }
+  name     = each.value.name
+
+  ports {
+    internal = each.value.ports.internal
+    external = each.value.ports.external
+  }
+  image   = docker_image.nginx.latest
+  restart = "no"
+}
