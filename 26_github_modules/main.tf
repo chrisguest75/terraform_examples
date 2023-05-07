0471f86decd2d57221c882ba0c83c87fc87b23da
Chris Guest <chrisguest75@users.noreply.github.com>
2023-05-07 19:56:46 +0100

    Fix modules example to not use ssh

--- 26_github_modules/main.tf
+++ 26_github_modules/main.tf
@@ -1,7 +1,7 @@
 module "my_redis1" {
   # NOTE: Normally you would not reference the same repo back to a previous version.  
   # This is just to demonstrate the use of a specific version of a module.
-  source = "git::git@github.com:chrisguest75/terraform_examples.git//08_module/modules/redis?ref=3ae2ca402d268c465998083ae19cb65787603674"
+  source = "git::https://github.com/chrisguest75/terraform_examples//08_module/modules/redis?ref=3ae2ca402d268c465998083ae19cb65787603674"
 
   name = "my_redis1"
   image = "redis:6.2-alpine"
@@ -10,7 +10,7 @@ module "my_redis1" {
 module "my_redis2" {
   # NOTE: Normally you would not reference the same repo back to a previous version.  
   # This is just to demonstrate the use of a specific version of a module.
-  source = "git::git@github.com:chrisguest75/terraform_examples.git//08_module/modules/redis?ref=3ae2ca402d268c465998083ae19cb65787603674"
+  source = "git::https://github.com/chrisguest75/terraform_examples//08_module/modules/redis?ref=3ae2ca402d268c465998083ae19cb65787603674"
 
   name = "my_redis2"
   image = "redis:6.2.11-alpine"
